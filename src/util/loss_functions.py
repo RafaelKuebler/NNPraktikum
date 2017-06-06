ceae64e72e9025644f7d87d1568cf2955ceb534a
Rainer Kartmann <rainer.kartmann@student.kit.edu>
2017-06-06 21:48:03 +0200

    Implemented remaining loss functions (not sure about cross entropies)

--- src/util/loss_functions.py
+++ src/util/loss_functions.py
@@ -60,7 +60,8 @@ class MeanSquaredError(Error):
 
     def calculateError(self, target, output):
         # MSE = 1/n*sum (i=1 to n) of (target_i - output_i)^2)
-        pass
+        # reuse SSE
+        return SumSquaredError().calculateError(target, output) / len(target)
 
 
 class SumSquaredError(Error):
@@ -73,7 +74,13 @@ class SumSquaredError(Error):
 
     def calculateError(self, target, output):
         # SSE = 1/2*sum (i=1 to n) of (target_i - output_i)^2)
-        pass
+        # assume target and output are lists
+        sum = 0
+        n = len(target)
+        for i in range(n):
+            sum += (target[i] - output[i]) ** 2
+        return sum
+
 
 
 class BinaryCrossEntropyError(Error):
@@ -85,7 +92,26 @@ class BinaryCrossEntropyError(Error):
         self.errorString = 'bce'
 
     def calculateError(self, target, output):
-        pass
+        # for one instance x: (taken from [ExerciseSlides, p. 3]
+        # E_bce = - sum(k) of (t_k*log(o_k) + (1-t_k)*log(1-o_k))
+        # => To calculate the error of one fixed instance,
+        #    we would need target and output to contain the values of
+        #    each output unit (=> be a list over output neurons)
+
+        # actually, I have no idea what the difference between BCE and CE is
+
+        def crossEntropyFixedK(target_k, output_k):
+            """Compute one element of the sum"""
+            return - (target_k * np.log(output_k) + (1-target_k) * np.log(1-output_k))
+
+        if type(target) is list:
+            sum = 0
+            for k in range(len(target)):
+                sum += crossEntropyFixedK(target[k], output[k])
+            return sum
+        else:
+            return crossEntropyFixedK(target, output)
+
 
 
 class CrossEntropyError(Error):
@@ -97,4 +123,23 @@ class CrossEntropyError(Error):
         self.errorString = 'crossentropy'
 
     def calculateError(self, target, output):
-        pass
+        # for one instance x: (taken from [2016Slides, p. 44])
+
+        # E_ce = - sum(k) of (t_k*log(o_k) + (1-t_k)*log(1-o_k))
+        # => To calculate the error of one fixed instance,
+        #    we would need target and output to contain the values of
+        #    each output unit (=> be a list over output neurons)
+
+        # actually, I have no idea what the difference between BCE and CE is
+
+        def crossEntropyFixedK(target_k, output_k):
+            """Compute one element of the sum."""
+            return - (target_k * np.log(output_k) + (1-target_k) * np.log(1-output_k))
+
+        if type(target) is list:
+            sum = 0
+            for k in range(len(target)):
+                sum += crossEntropyFixedK(target[k], output[k])
+            return sum
+        else:
+            return crossEntropyFixedK(target, output)
