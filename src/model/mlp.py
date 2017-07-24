07c47fe2262a406d53a603d1a5ddb34319659f2a
thanhleha <thanhlehavn@gmail.com>
2017-07-24 21:10:50 +0200

    Skeleton code for Ex4 - MLP

---
+++ src/model/mlp.py
@@ -0,0 +1,176 @@
+
+import numpy as np
+
+from util.loss_functions import CrossEntropyError
+from model.logistic_layer import LogisticLayer
+from model.classifier import Classifier
+
+from sklearn.metrics import accuracy_score
+
+import sys
+
+class MultilayerPerceptron(Classifier):
+    """
+    A multilayer perceptron used for classification
+    """
+
+    def __init__(self, train, valid, test, layers=None, inputWeights=None,
+                 outputTask='classification', outputActivation='softmax',
+                 loss='crossentropy', learningRate=0.01, epochs=50):
+
+        """
+        A MNIST recognizer based on multi-layer perceptron algorithm
+
+        Parameters
+        ----------
+        train : list
+        valid : list
+        test : list
+        learningRate : float
+        epochs : positive int
+
+        Attributes
+        ----------
+        trainingSet : list
+        validationSet : list
+        testSet : list
+        learningRate : float
+        epochs : positive int
+        performances: array of floats
+        """
+
+        self.learningRate = learningRate
+        self.epochs = epochs
+        self.outputTask = outputTask  # Either classification or regression
+        self.outputActivation = outputActivation
+        self.cost = cost
+
+        self.trainingSet = train
+        self.validationSet = valid
+        self.testSet = test
+        
+        if loss == 'bce':
+            self.loss = BinaryCrossEntropyError()
+        elif loss == 'sse':
+            self.loss = SumSquaredError()
+        elif loss == 'mse':
+            self.loss = MeanSquaredError()
+        elif loss == 'different':
+            self.loss = DifferentError()
+        elif loss == 'absolute':
+            self.loss = AbsoluteError()
+        else:
+            raise ValueError('There is no predefined loss function ' +
+                             'named ' + str)
+
+        # Record the performance of each epoch for later usages
+        # e.g. plotting, reporting..
+        self.performances = []
+
+        self.layers = layers
+
+        # Build up the network from specific layers
+        self.layers = []
+
+        # Input layer
+        inputActivation = "sigmoid"
+        self.layers.append(LogisticLayer(train.input.shape[1], 128, 
+                           None, inputActivation, False))
+
+        # Output layer
+        outputActivation = "softmax"
+        self.layers.append(LogisticLayer(128, 10, 
+                           None, outputActivation, True))
+
+        self.inputWeights = inputWeights
+
+        # add bias values ("1"s) at the beginning of all data sets
+        self.trainingSet.input = np.insert(self.trainingSet.input, 0, 1,
+                                            axis=1)
+        self.validationSet.input = np.insert(self.validationSet.input, 0, 1,
+                                              axis=1)
+        self.testSet.input = np.insert(self.testSet.input, 0, 1, axis=1)
+
+
+    def _get_layer(self, layer_index):
+        return self.layers[layer_index]
+
+    def _get_input_layer(self):
+        return self._get_layer(0)
+
+    def _get_output_layer(self):
+        return self._get_layer(-1)
+
+    def _feed_forward(self, inp):
+        """
+        Do feed forward through the layers of the network
+
+        Parameters
+        ----------
+        inp : ndarray
+            a numpy array containing the input of the layer
+
+        # Here you have to propagate forward through the layers
+        # And remember the activation values of each layer
+        """
+        
+    def _compute_error(self, target):
+        """
+        Compute the total error of the network (error terms from the output layer)
+
+        Returns
+        -------
+        ndarray :
+            a numpy array (1,nOut) containing the output of the layer
+        """
+        pass
+    
+    def _update_weights(self, learningRate):
+        """
+        Update the weights of the layers by propagating back the error
+        """
+        pass
+        
+    def train(self, verbose=True):
+        """Train the Multi-layer Perceptrons
+
+        Parameters
+        ----------
+        verbose : boolean
+            Print logging messages with validation accuracy if verbose is True.
+        """
+        pass
+
+
+
+    def classify(self, test_instance):
+        # Classify an instance given the model of the classifier
+        # You need to implement something here
+        pass
+        
+
+    def evaluate(self, test=None):
+        """Evaluate a whole dataset.
+
+        Parameters
+        ----------
+        test : the dataset to be classified
+        if no test data, the test set associated to the classifier will be used
+
+        Returns
+        -------
+        List:
+            List of classified decisions for the dataset's entries.
+        """
+        if test is None:
+            test = self.testSet.input
+        # Once you can classify an instance, just use map for all of the test
+        # set.
+        return list(map(self.classify, test))
+
+    def __del__(self):
+        # Remove the bias from input data
+        self.trainingSet.input = np.delete(self.trainingSet.input, 0, axis=1)
+        self.validationSet.input = np.delete(self.validationSet.input, 0,
+                                              axis=1)
+        self.testSet.input = np.delete(self.testSet.input, 0, axis=1)
---
+++ src/report/performance_plot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+
+class PerformancePlot(object):
+    '''
+    Class to plot the performances
+    Very simple and badly-styled
+    '''
+
+    def __init__(self, name):
+        '''
+        Constructor
+        '''
+        self.name = name
+
+    def draw_performance_epoch(self, performances, epochs):
+        plt.plot(range(epochs), performances, 'k',
+                 range(epochs), performances, 'ro')
+        plt.title("Performance of " + self.name + " over the epochs")
+        plt.ylim(ymax=1)
+        plt.ylabel("Accuracy")
+        plt.xlabel("Epoch")
+        plt.show()
